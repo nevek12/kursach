0a62d59cd930afdbb4a0f33b83375a4e0c3a2c3a
nevek12 <vladnevvk@mail.ru>
2025-05-08 11:14:55 +0300

    добавлено скрытие уязвимых данных

--- kursach/kursach/settings.py
+++ kursach/kursach/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
 
+env = Env()
+env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pa*$32^s$qbeslolz&sjtk3!wepv(#&n9b9bap%wn595i9gr#)'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,9 +89,9 @@ WSGI_APPLICATION = 'kursach.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kursach',
-        'USER': 'postgres',
-        'PASSWORD': 'nevek12042005',
+        'NAME': env('NAME'),
+        'USER': env('USER'),
+        'PASSWORD': env('PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
